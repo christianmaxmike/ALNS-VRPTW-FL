08e7c080b450b6786839eb778ec0198823230552
Doegstra <jungwirth.research@gmail.com>
2021-05-23 22:14:28 +0200

    test outlined for solomon instance generator

---
+++ test/vrptwfl/metaheuristic/instanceGenerator/SolomonInstanceGeneratorTest.java
@@ -0,0 +1,9 @@
+package vrptwfl.metaheuristic.instanceGenerator;
+
+public class SolomonInstanceGeneratorTest {
+
+    // test, dass Fehler geworfen wird, wenn ungueltiger Pfadname uebergeben
+
+    // test, dass falls Pfad gueltig ist, die richtigen Infos an der richtigen Stelle stehen
+    // hier brauchen wir ein Datenobjekt
+}
